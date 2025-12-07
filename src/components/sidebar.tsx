7399d9d4b6ce6e6daf1a334c5fa5da0795d75e18
sai <saikrishna@shristitech.com>
2025-12-07 19:03:20 +0000

    fix sidenav error

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,7 +8,7 @@ import { WorkspaceSwitcher } from './workspaces-switcher';
 
 export const Sidebar = () => {
   return (
-    <aside className="size-full bg-neutral-100 p-4">
+    <aside className="flex min-h-screen flex-col bg-neutral-100 p-4">
       <Logo />
 
       <DottedSeparator className="my-4" />
