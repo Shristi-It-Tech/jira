b1e18c33b89b33962f1d9b952db9dcf95e78ec63
sai <saikrishna@shristitech.com>
2025-12-07 11:25:26 +0000

    ui refinement remove git link

--- src/components/source-code.tsx
+++ src/components/source-code.tsx
@@ -4,9 +4,5 @@ import Link from 'next/link';
 import { links } from '@/config';
 
 export const SourceCode = () => {
-  return (
-    <Link href={links.sourceCode} target="_blank" rel="noreferrer noopener" className="hover:opacity-80" title="Source Code">
-      <Image src="/github.svg" alt="GitHub" height={30} width={30} />
-    </Link>
-  );
+  return <Link href={links.sourceCode} target="_blank" rel="noreferrer noopener" className="hover:opacity-80" title="Source Code"></Link>;
 };
